ee93e4aaf02f85327e9a11939cf2964a0f7eb6b3
Sebas09210426 <sebasgalvis0426@gmail.com>
2026-09-06 18:40:07 -0500

    Ejemplo

--- TrabajoA1Programacion2/src/main/java/org/example/Main.java
+++ TrabajoA1Programacion2/src/main/java/org/example/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("Hola! Soy dev_Sebas");
         System.out.println("Hola! Soy dev_Camilo");
         System.out.println("hola soy dev_Valeria");
+
+        System.out.println("Mensaje de ejemplo");
     }
 }
